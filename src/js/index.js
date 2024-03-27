7e9cbdcbe69db7509fd0e9affbecaad1c8295be7
PeterKohlichhaus <peter.kohlichhaus@gmail.com>
2024-03-27 10:02:18 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -130,10 +130,16 @@ const debounce = (func, delay= 500, immediate=false) => {
 // detect clock settings
 hourCycle = Intl.DateTimeFormat(locale, {hour: 'numeric'}).resolvedOptions().hourCycle;
 
-function displayClock() {
+function displayClockOLD() {
     clock.textContent = new Date().toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hourCycle: hourCycle});
     setTimeout(displayClock, 10000);
 }
+
+function displayClock() {
+    clock.textContent = new Date().toLocaleString(navigator.language, {hour: 'numeric', minute: 'numeric', hourCycle: 'h23'});
+    setTimeout(displayClock, 10000);
+}
+
 
 displayClock();
 
